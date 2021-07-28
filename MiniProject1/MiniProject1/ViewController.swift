89311a649adc17848b55817d47fec466b040fc07
yayam15 <40362526+yayam15@users.noreply.github.com>
2021-07-28 16:56:20 -0400

    3rd commit

--- MiniProject1/MiniProject1/ViewController.swift
+++ MiniProject1/MiniProject1/ViewController.swift
@@ -9,17 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var firstFacts = {
-        "My family originates from Guatemala"; "I'm addicted to exercising"; "I grew up in Maryland"
-    }
+    var firstFacts = [
+        "My family originates from Guatemala", "I'm addicted to exercising", "I grew up in Maryland"
+    ]
     
-    var secondFacts = {
-        "This is my third time at Kode with Klossy"; "I have attended a hackathon called Technica"; "I want to study Cyber Security"
-    }
+    var secondFacts = [
+        "This is my third time at Kode with Klossy", "I have attended a hackathon called Technica", "I want to study Cyber Security"
+    ]
     
-    var thirdFacts = {
-        "I attend an online charter school called CCA"; "I am the oldest of three"; "I'm goign to be a senior in high school"
-    }
+    var thirdFacts = [
+        "I attend an online charter school called CCA", "I am the oldest of three", "I'm goign to be a senior in high school"
+    ]
     
     @IBOutlet weak var fact1: UILabel!
     @IBOutlet weak var fact2: UILabel!
@@ -27,24 +27,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        fact1.isHidden = true
-        fact2.isHidden = true
-        fact3.isHidden = true
+       
     }
 
     @IBAction func clickButton(_ sender: Any) {
         let randomIndex1 = Int.random(in: 0..<firstFacts.count)
-        let fact1 = firstFacts[randomIndex1]
-        firstFacts.text = fact1
+        let factOne = firstFacts[randomIndex1]
+        fact1.text = factOne
         
         let randomIndex2 = Int.random(in: 0..<secondFacts.count)
-        let fact2 = firstFacts[randomIndex2]
-        secondFacts.text = fact2
+        let factTwo = firstFacts[randomIndex2]
+        fact2.text = factTwo
         
         let randomIndex3 = Int.random(in: 0..<thirdFacts.count)
-        let fact3 = firstFacts[randomIndex3]
-        thirdFacts.text = fact3
+        let factThree = firstFacts[randomIndex3]
+        fact3.text = factThree
     }
     
 }
