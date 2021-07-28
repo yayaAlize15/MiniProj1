4f5be5e20413ac2317da20476df98c5610c4e0f5
yayam15 <40362526+yayam15@users.noreply.github.com>
2021-07-28 16:05:52 -0400

    2nd commit

--- MiniProject1/MiniProject1/ViewController.swift
+++ MiniProject1/MiniProject1/ViewController.swift
@@ -9,8 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var facts = {
-        "My family originates from Guatemala", "I'm addicted to exercising","I grew up in Maryland"
+    var firstFacts = {
+        "My family originates from Guatemala"; "I'm addicted to exercising"; "I grew up in Maryland"
+    }
+    
+    var secondFacts = {
+        "This is my third time at Kode with Klossy"; "I have attended a hackathon called Technica"; "I want to study Cyber Security"
+    }
+    
+    var thirdFacts = {
+        "I attend an online charter school called CCA"; "I am the oldest of three"; "I'm goign to be a senior in high school"
     }
     
     @IBOutlet weak var fact1: UILabel!
@@ -19,19 +27,24 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         fact1.isHidden = true
         fact2.isHidden = true
         fact3.isHidden = true
     }
 
     @IBAction func clickButton(_ sender: Any) {
-        let randomIndex = Int.random(in: 0..<facts.count)
-        let randomFact = facts[randomIndex]
+        let randomIndex1 = Int.random(in: 0..<firstFacts.count)
+        let fact1 = firstFacts[randomIndex1]
+        firstFacts.text = fact1
         
-        fact1.text = randomFact
-        fact2.text = randomFact
-        fact3.text = randomFact
+        let randomIndex2 = Int.random(in: 0..<secondFacts.count)
+        let fact2 = firstFacts[randomIndex2]
+        secondFacts.text = fact2
+        
+        let randomIndex3 = Int.random(in: 0..<thirdFacts.count)
+        let fact3 = firstFacts[randomIndex3]
+        thirdFacts.text = fact3
     }
     
 }
